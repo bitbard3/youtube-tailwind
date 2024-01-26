fcec51ecb2e985388082c60b7447580265ba5910
bitbard3 <ansharora3839@gmail.com>
2024-01-27 01:45:15 +0530

    Add Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { VideoGallery } from "./components/VideoGallery";
 import { Navbar } from "./components/NavBar";
+import { SideBar } from "./components/SideBar";
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      <VideoGallery></VideoGallery>
+      <div className="flex">
+        <SideBar></SideBar>
+        <VideoGallery></VideoGallery>
+      </div>
     </>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,112 @@
+import { SideBarOptions } from "./SideBarOptions";
+
+export const SideBar = () => {
+  return (
+    <div className="">
+      <div className="flex flex-col pl-1.5 pr-5">
+        <div className="flex flex-col items-center mt-0">
+          <SideBarOptions
+            text={"Home"}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.0}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+            }
+          ></SideBarOptions>
+          <SideBarOptions
+            text={"Shorts"}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.0}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                />
+              </svg>
+            }
+          ></SideBarOptions>
+          <SideBarOptions
+            text={"Subscription"}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.0}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"
+                />
+              </svg>
+            }
+          ></SideBarOptions>
+          <SideBarOptions
+            text={"You"}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.0}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
+                />
+              </svg>
+            }
+          ></SideBarOptions>
+          <SideBarOptions
+            text={"History"}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.0}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            }
+          ></SideBarOptions>
+        </div>
+      </div>
+    </div>
+  );
+};
