e84f05214e258d558501899df6de61c99582550b
bitbard3 <ansharora3839@gmail.com>
2024-01-30 03:18:52 +0530

    Add functionality to toggle sidebar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,34 @@
+import { useSetRecoilState } from "recoil";
 import { AccountSection } from "./AccountSection";
 import { BrandIcon } from "./BrandIcon";
 import { Searchbox } from "./SearchBox";
+import { sidebarAtom } from "../store/atoms/sideBarAtom";
 export const Navbar = () => {
+  const setSidebar = useSetRecoilState(sidebarAtom);
+  const SideBarClickHandler = () => {
+    setSidebar((state) => !state);
+  };
   return (
     <div className="">
       <div className="flex justify-between mx-5">
         <div className="  pt-5 flex content-center">
           <div className="mr-7">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={0.8}
-              stroke="currentColor"
-              className="w-6 h-6 -mt-0.5 "
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-              />
-            </svg>
+            <button onClick={SideBarClickHandler}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={0.8}
+                stroke="currentColor"
+                className="w-6 h-6 -mt-0.5 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                />
+              </svg>
+            </button>
           </div>
           <div className="w-24 h-10">
             <BrandIcon></BrandIcon>
--- src/components/SideBarOptions.jsx
+++ src/components/SideBarOptions.jsx
@@ -1,6 +1,12 @@
+import { useRecoilValue } from "recoil";
+import { sidebarAtom } from "../store/atoms/sideBarAtom";
+
 export const SideBarOptions = ({ icon, text }) => {
+  const sideBar = useRecoilValue(sidebarAtom);
   return (
-    <div className="flex flex-col items-center mt-5">
+    <div
+      className={`flex flex-col items-center mt-5 ${sideBar ? `` : `hidden`}`}
+    >
       {icon}
       <span className="text-[10px]">{text}</span>
     </div>
